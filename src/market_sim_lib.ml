a7c966550024391f1fb4c0031233c7f66ba49908
Dante Basile <dbasile1070@gmail.com>
2021-11-10 16:52:44 -0500

    prototype functions up to buy_ipo

--- src/market_sim_lib.ml
+++ src/market_sim_lib.ml
@@ -5,9 +5,9 @@ open Core
 (*
   Contains a ticker for all companies listed on the market
   Maps ticker to list of past and present stock prices
-  string -> float list
+  string -> float option list
 *)
-type stock_price_map = (float, string) result list Map.M(String).t
+type stock_price_map = float option list Map.M(String).t
 
 (*
   Maps ticker to past and present shares owned
@@ -18,18 +18,18 @@ type stock_ct_map = int list Map.M(String).t
 (*
   Record to store funds and stocks of player
 *)
-type player = {funds: int list; stocks: stock_ct_map}
+type player = {funds: float list; stocks: stock_ct_map}
 
 (*
   Maps id to player
-  int -> player
+  string -> player
 *)
-type player_map = player Map.M(Int).t 
+type player_map = player Map.M(String).t 
 
 (*
   Record to store details of order
 *)
-type order = {id: int; qty: int; value: float}
+type order = {id: string; ct: int; value: float}
 
 (*
   Maps ticker to list of relevant order tuples
@@ -41,19 +41,53 @@ type order_map = order list Map.M(String).t
   Add stock to stock_price_map
 *)
 let add_stock (ticker: string) (stocks: stock_price_map): (stock_price_map, string) result =
-  failwith "unimplemented"
+  match Map.add stocks ~key:ticker ~data:([None]) with
+  | `Ok stocks_new -> Ok stocks_new
+  | `Duplicate -> Error "stock already exists"
 
 (*
   Add player to player_map
 *)
-let add_player (id: int) (p: player) (pm: player_map): (player_map, string) result =
-  failwith "unimplemented"
+let add_player (id: string) (pm: player_map): (player_map, string) result =
+  match Map.add pm ~key:id ~data:{funds = [0.]; stocks = Map.empty (module String)} with
+  | `Ok pm_new -> Ok pm_new
+  | `Duplicate -> Error "player already exists"
+
+let buy (ticker: string) (o: order) (players: player_map): (player_map, string) result =
+  let inc_stock_ct (ct_add: int) (l_ct: int list option): int list =
+    match l_ct with
+    | Some l -> 
+      let new_ct = 
+        match l with
+        current_ct :: l_r -> (ct_add + current_ct)
+        | [] -> failwith "player stocks has no ct"
+      in
+      new_ct :: l
+    | None -> [ct_add]
+  in
+  match Map.find players o.id with
+  | Some p -> 
+    let new_amt = 
+      match p.funds with
+      | current_amt :: funds_r -> current_amt -. (Float.of_int o.ct) *. o.value
+      | [] -> failwith "player has no current funds"
+    in
+    if Float.(>=) new_amt 0. then
+      let new_funds = new_amt :: p.funds in
+      let new_stocks = Map.update p.stocks ticker ~f:(inc_stock_ct o.ct) in
+      let new_player = {funds = new_funds; stocks = new_stocks} in
+      Ok (Map.set players ~key:o.id ~data:new_player)
+    else
+      Error "insufficient funds"
+  | None -> Error "player does not exist"
 
 (*
   Player acquires stock from IPO
 *)
-let buy_ipo (ticker: string) (o: order): (player_map, string) result =
-  failwith "unimplemented"
+let buy_ipo (ticker: string) (o: order) (stocks: stock_price_map) (players: player_map): (player_map, string) result =
+  match Map.find stocks ticker with
+  | Some _ -> buy ticker o players
+  | None -> Error "stock not listed on market"
 
 (*
   Attempt to find an ask matching this bid and conduct the transaction

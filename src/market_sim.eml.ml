2c5904770278dad1a83f05b86094e428f9ba6c8f
Jocelyn Hsu <jocelynhsu8@gmail.com>
2021-12-06 21:48:09 -0500

    choose action with text and populate elements accordingly

--- src/market_sim.eml.ml
+++ src/market_sim.eml.ml
@@ -5,40 +5,71 @@
 open Market_sim_lib;;
 open Core;;
 
-let render_stock = 
-  <html>
-  <body>
-  % print_string "hi"
-    <h1>Add stock: </h1>
-    <input type="text">
-    <button>Submit</button>
-  </body>
-  </html>
-
-let render_home tasks =
+let render_home ?action request =
   <html>
   <body>
     <h1>Stock Market Simulator</h1>
-    <button>Add stock</button>
+
+%   begin match action with
+%   | None -> 
+      <%s! Dream.form_tag ~action:"/" request %>
+        <p>Please choose one of the following options and indicate your choice below: </p>
+        <li>Add stock</li>
+        <li>Add player</li>
+        <li>Buy IPO</li>
+        <li>Bid offer</li>
+        <li>Ask offer</li>
+        <li>Get price</li>
+        <li>Get player info</li>
+
+        <br>
+        <input name="action" autofocus>
+      </form>
+%   | Some action -> match String.lowercase action with
+%     | "add stock" -> 
+        <h1>Add stock: </h1>
+%     | "add player" -> 
+        <h1>Add player: </h1>
+%     | _ -> 
+      <p>Invalid option</p>
+      <%s! Dream.form_tag ~action:"/" request %>
+        <p>Please choose one of the following options and indicate your choice below: </p>
+        <li>Add stock</li>
+        <li>Add player</li>
+        <li>Buy IPO</li>
+        <li>Bid offer</li>
+        <li>Ask offer</li>
+        <li>Get price</li>
+        <li>Get player info</li>
+
+        <br>
+        <input name="action" autofocus>
+      </form>
+      
+%   end;
+
+    
+    
   </body>
   </html> 
-
-
-let tasks = [
-  ("write documentation", true);
-  ("create examples", true);
-  ("publish website", true);
-  ("profit", false);
-] 
 
 let () =
   Dream.run
   @@ Dream.logger
+  @@ Dream.memory_sessions
   @@ Dream.router [
-    Dream.get "/"
-      (fun _ ->
-        render_home tasks
-        |> Dream.html);
+
+    Dream.get  "/"
+      (fun request ->
+        Dream.html (render_home request));
+
+    Dream.post "/"
+      (fun request ->
+        match%lwt Dream.form request with
+        | `Ok ["action", action] ->
+          Dream.html (render_home ~action request)
+        | _ ->
+          Dream.empty `Bad_Request);
         
 
 

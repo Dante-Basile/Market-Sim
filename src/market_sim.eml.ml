7f991936d996cbe22ddd2a75912429b688e6a6f4
Jocelyn Hsu <jocelynhsu8@gmail.com>
2021-12-07 16:51:43 -0500

    render different page for different action

--- src/market_sim.eml.ml
+++ src/market_sim.eml.ml
@@ -5,61 +5,54 @@
 open Market_sim_lib;;
 open Core;;
 
+let render_stock ?action request =
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Stock Market Simulator</title>
+  </head>
+  <body>
+    <h1>Add Stock</h1>
+  
+    <%s! Dream.form_tag ~action:"/" request %>
+      <input name="action" autofocus>
+    </form>
+      
+  </body>
+  </html> 
+
 let render_home ?action request =
   <html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Stock Market Simulator</title>
+  </head>
   <body>
     <h1>Stock Market Simulator</h1>
+    <script src="./client.js" id="root"></script>
 
 %   begin match action with
-%   | None -> 
-      <%s! Dream.form_tag ~action:"/" request %>
-        <p>Please choose one of the following options and indicate your choice below: </p>
-        <li>Add stock</li>
-        <li>Add player</li>
-        <li>Buy IPO</li>
-        <li>Bid offer</li>
-        <li>Ask offer</li>
-        <li>Get price</li>
-        <li>Get player info</li>
-
-        <br>
-        <input name="action" autofocus>
-      </form>
-%   | Some action -> match String.lowercase action with
-%     | "add stock" -> 
-        <h1>Add stock: </h1>
-%     | "add player" -> 
-        <h1>Add player: </h1>
-%     | "add buy ipo" -> 
-        <h1>Buy IPO: </h1>
-%     | "bid offer" -> 
-        <h1>Bid offer: </h1>
-%     | "ask offer" -> 
-        <h1>Ask offer: </h1>
-%     | "get price" -> 
-        <h1>Get price: </h1>
-%     | "get player" -> 
-        <h1>Get player: </h1>
-%     | _ -> 
+%   | None -> ()
+%   | Some action ->
       <p>Invalid option</p>
-      <%s! Dream.form_tag ~action:"/" request %>
-        <p>Please choose one of the following options and indicate your choice below: </p>
-        <li>Add stock</li>
-        <li>Add player</li>
-        <li>Buy IPO</li>
-        <li>Bid offer</li>
-        <li>Ask offer</li>
-        <li>Get price</li>
-        <li>Get player info</li>
-
-        <br>
-        <input name="action" autofocus>
-      </form>
+     
       
 %   end;
+  <%s! Dream.form_tag ~action:"/" request %>
+  <p>Please choose one of the following options and indicate your choice below: </p>
+  <li>Add stock</li>
+  <li>Add player</li>
+  <li>Buy IPO</li>
+  <li>Bid offer</li>
+  <li>Ask offer</li>
+  <li>Get price</li>
+  <li>Get player info</li>
 
-    
-    
+  <br>
+  <input name="action" autofocus>
+  </form>
   </body>
   </html> 
 
@@ -84,8 +77,9 @@ let () =
     Dream.post "/"
       (fun request ->
         match%lwt Dream.form request with
-        | `Ok ["action", action] ->
-          Dream.html (render_home ~action request)
+        | `Ok ["action", "add stock"] ->
+          Dream.html (render_stock request)
+        | `Ok ["action", action] -> Dream.html (render_home ~action request)
         | _ ->
           Dream.empty `Bad_Request);
         
